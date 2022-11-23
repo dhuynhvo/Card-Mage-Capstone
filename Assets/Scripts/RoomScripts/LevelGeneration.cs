466fc6aee4119cdd1b718893ec83919f1d01b5a8
Rbothne <70356932+Rbothne@users.noreply.github.com>
2022-11-22 23:45:52 -0800

    fixing my mistakes
    new scene map gen

--- Assets/Scripts/RoomScripts/LevelGeneration.cs
+++ Assets/Scripts/RoomScripts/LevelGeneration.cs
@@ -10,6 +10,7 @@ using Random = UnityEngine.Random;
 public class LevelGeneration : MonoBehaviour
 {
     public Transform[] startingPositions;
+    public Transform playerLoc;
     public GameObject[] rooms; //index 0 = LR, index 1 = LRB, index 2 = LRT, index 3 = LRBT
     public Transform[] poses;
 
@@ -17,17 +18,16 @@ public class LevelGeneration : MonoBehaviour
     public float moveAmount; //offset for spawning
 
     private float timeBetweenRoom;//time to spawn a room
-    public float startTimeBetweenRoom = 0.5f; 
+    public float startTimeBetweenRoom = 0.5f;
 
     public float minX; //farthest left can gen
     public float maxX; //farthest right can gen
     public float minZ; //farthest down can gen
     public bool stopGen; //kill generator
 
-    private int downCount=0;// if going down twice, spawn a room with holes in every direction
+    private int downCount = 0;// if going down twice, spawn a room with holes in every direction
     private int randStartingPos;
     public LayerMask room;
-    private Collider2D roomDetection;
     public LayerMask Pose;
     private Collider[] poseDetection;
 
@@ -38,16 +38,17 @@ public class LevelGeneration : MonoBehaviour
     {
         randStartingPos = Random.Range(0, startingPositions.Length);
         transform.position = startingPositions[randStartingPos].position;
-        Instantiate(rooms[0],transform.position, Quaternion.identity);
+        playerLoc.transform.position = startingPositions[randStartingPos].position;
+        Instantiate(rooms[0], transform.position, Quaternion.identity);
         poseDetection = Physics.OverlapSphere(transform.position, 1, Pose);
         Destroy(poseDetection[0].gameObject);
-        direction = Random.Range(1,6);
+        direction = Random.Range(1, 6);
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (timeBetweenRoom <= 0 && stopGen==false)
+        if (timeBetweenRoom <= 0 && stopGen == false)
         {
             Move();
             timeBetweenRoom = startTimeBetweenRoom;
@@ -57,12 +58,13 @@ public class LevelGeneration : MonoBehaviour
             timeBetweenRoom -= Time.deltaTime;
         }
     }
-        
+
     private void Move()
     {
         if (direction == 1 || direction == 2)//move stamp RIGHT
         {
-            if (transform.position.x < maxX){
+            if (transform.position.x < maxX)
+            {
                 downCount = 0;
                 Vector3 newPos = new Vector3(transform.position.x + moveAmount, transform.position.y, transform.position.z);
                 transform.position = newPos;
@@ -70,7 +72,7 @@ public class LevelGeneration : MonoBehaviour
                 int rand = Random.Range(0, rooms.Length);
                 Instantiate(rooms[rand], transform.position, Quaternion.identity);
                 poseDetection = Physics.OverlapSphere(transform.position, 1, Pose);
-                if (poseDetection != null && poseDetection.Length>0)
+                if (poseDetection != null && poseDetection.Length > 0)
                 {
                     if (poseDetection[0] != null)
                     {
@@ -93,10 +95,10 @@ public class LevelGeneration : MonoBehaviour
             {
                 direction = 5;
             }
-        } 
-        else if(direction == 3 || direction == 4)//move stampLEFT
+        }
+        else if (direction == 3 || direction == 4)//move stampLEFT
         {
-            if (transform.position.x >minX)
+            if (transform.position.x > minX)
             {
                 downCount = 0;
                 Vector3 newPos = new Vector3(transform.position.x - moveAmount, transform.position.y, transform.position.z);
@@ -106,7 +108,7 @@ public class LevelGeneration : MonoBehaviour
                 int rand = Random.Range(0, rooms.Length);
                 Instantiate(rooms[rand], transform.position, Quaternion.identity);
                 poseDetection = Physics.OverlapSphere(transform.position, 1, Pose);
-                if (poseDetection != null && poseDetection.Length>0)
+                if (poseDetection != null && poseDetection.Length > 0)
                 {
                     if (poseDetection[0] != null)
                     {
@@ -114,14 +116,14 @@ public class LevelGeneration : MonoBehaviour
                         Destroy(poseDetection[0].gameObject);
                     }
                 }
-                
+
             }
             else
-            { 
-                direction = 5; 
-            }          
+            {
+                direction = 5;
+            }
         }
-        
+
         else if (direction == 5)//move DOWN
         {
             downCount++;
@@ -134,9 +136,9 @@ public class LevelGeneration : MonoBehaviour
                     if (downCount >= 2)
                     {
                         roomDetection.GetComponent<RoomType>().RoomDestruction();// destroy a room
-                        Instantiate(rooms[3], transform.position,Quaternion.identity);
+                        Instantiate(rooms[3], transform.position, Quaternion.identity);
                         poseDetection = Physics.OverlapSphere(transform.position, 1, Pose);
-                        if (poseDetection != null && poseDetection.Length>0)
+                        if (poseDetection != null && poseDetection.Length > 0)
                         {
                             if (poseDetection[0] != null)
                             {
@@ -158,27 +160,27 @@ public class LevelGeneration : MonoBehaviour
                         Instantiate(rooms[randBottomRoom], transform.position, Quaternion.identity);
 
                         poseDetection = Physics.OverlapSphere(transform.position, 1, Pose);
-                        if (poseDetection != null && poseDetection.Length>0)
+                        if (poseDetection != null && poseDetection.Length > 0)
                         {
-                            if (poseDetection[0]!= null)
+                            if (poseDetection[0] != null)
                             {
                                 Debug.Log("Pose to Destroy" + poseDetection[0].name);
                                 Destroy(poseDetection[0].gameObject);
                             }
-                            
+
                         }
-                        
+
                     }
-                        
+
                 }
                 Vector3 newPos = new Vector3(transform.position.x, transform.position.y, transform.position.z - moveAmount);
                 transform.position = newPos;
                 direction = Random.Range(1, 6);
 
-                int rand = Random.Range(2,4);
+                int rand = Random.Range(2, 4);
                 Instantiate(rooms[rand], transform.position, Quaternion.identity);
                 poseDetection = Physics.OverlapSphere(transform.position, 1, Pose);
-                if (poseDetection != null && poseDetection.Length>0)
+                if (poseDetection != null && poseDetection.Length > 0)
                 {
                     if (poseDetection[0] != null)
                     {
@@ -201,8 +203,7 @@ public class LevelGeneration : MonoBehaviour
                         Destroy(Pose.gameObject);
                     }
                 }
-
             }
         }
-    } 
+    }
 }
