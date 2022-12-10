7a24f97ba2d4e282f7ae88ecfb43bb37dcec0eab
Huynhvo_Dan <66800623+dhuynhvo@users.noreply.github.com>
2022-12-10 13:32:17 -0800

    Dash fix

--- Assets/Scripts/PlayerScripts/Player_Movement.cs
+++ Assets/Scripts/PlayerScripts/Player_Movement.cs
@@ -61,7 +61,7 @@ public class Player_Movement : MonoBehaviour
             MoveDir = NW;
             //rb.moveposition(transform.position + nw * time.deltatime * playerspeed * .7f);
             //playeravatar.transform.rotation = quaternion.euler(90, -45, 0);
-            //facingwhat = "ul";
+            FacingWhat = "ul";
         }
 
         else if (Input.GetKey(UpKey) && Input.GetKey(RightKey))
@@ -70,7 +70,7 @@ public class Player_Movement : MonoBehaviour
 
             //rb.MovePosition(transform.position + NE * Time.deltaTime * PlayerSpeed * .7f);
             //PlayerAvatar.transform.rotation = Quaternion.Euler(90, 45, 0);
-            //FacingWhat = "ur";
+            FacingWhat = "ur";
         }
 
         else if (Input.GetKey(DownKey) && Input.GetKey(LeftKey))
@@ -78,7 +78,7 @@ public class Player_Movement : MonoBehaviour
             MoveDir = SW;
             //rb.MovePosition(transform.position + SW * Time.deltaTime * PlayerSpeed * .7f);
             //PlayerAvatar.transform.rotation = Quaternion.Euler(90, -135, 0);
-            //FacingWhat = "dl";
+            FacingWhat = "dl";
         }
 
         else if (Input.GetKey(DownKey) && Input.GetKey(RightKey))
@@ -86,7 +86,7 @@ public class Player_Movement : MonoBehaviour
             MoveDir = SE;
             //rb.MovePosition(transform.position + SE * Time.deltaTime * PlayerSpeed * .7f);
             //PlayerAvatar.transform.rotation = Quaternion.Euler(90, 135, 0);
-            //FacingWhat = "dr";
+            FacingWhat = "dr";
         }
 
         else if (Input.GetKey(UpKey))
@@ -94,7 +94,7 @@ public class Player_Movement : MonoBehaviour
             MoveDir = Vector3.forward;
             //rb.MovePosition(transform.position + Vector3.forward * Time.deltaTime * PlayerSpeed);
             //PlayerAvatar.transform.rotation = Quaternion.Euler(90, 0, 0);
-            //FacingWhat = "u";
+            FacingWhat = "u";
         }
 
         else if (Input.GetKey(DownKey))
@@ -102,7 +102,7 @@ public class Player_Movement : MonoBehaviour
             MoveDir = Vector3.back;
             //rb.MovePosition(transform.position + Vector3.back * Time.deltaTime * PlayerSpeed);
             //PlayerAvatar.transform.rotation = Quaternion.Euler(90, 180, 0);
-            //FacingWhat = "d";
+            FacingWhat = "d";
         }
 
         else if (Input.GetKey(LeftKey))
@@ -110,14 +110,14 @@ public class Player_Movement : MonoBehaviour
             MoveDir = Vector3.left;
             //rb.MovePosition(transform.position + Vector3.left * Time.deltaTime * PlayerSpeed);
             //PlayerAvatar.transform.rotation = Quaternion.Euler(90, 270, 0);
-            //FacingWhat = "l";
+            FacingWhat = "l";
         }
         else if (Input.GetKey(RightKey))
         {
             MoveDir = Vector3.right;
             //rb.MovePosition(transform.position + Vector3.right * Time.deltaTime * PlayerSpeed);
             //PlayerAvatar.transform.rotation = Quaternion.Euler(90, 90, 0);
-            //FacingWhat = "r";
+            FacingWhat = "r";
         }
         
         if(Input.GetKeyDown(Dash) && (!IsDashing))
@@ -125,19 +125,24 @@ public class Player_Movement : MonoBehaviour
             Vector3 dash = DashDirection(FacingWhat);
             if (dash == NW || dash == NE || dash == SW || dash == SE)
             {
-                rb.AddForce(dash * DashSpeed * .7f);
+                rb.velocity = MoveDir * PlayerSpeed * DashSpeed;
                 IsDashing = true;
                 DashSphere.SetActive(true);
             }
 
             else
             {
-                rb.AddForce(dash * DashSpeed);
+                rb.velocity = MoveDir * PlayerSpeed * DashSpeed;
                 IsDashing = true;
                 DashSphere.SetActive(true);
             }
         }
-        rb.velocity = MoveDir * PlayerSpeed;
+        
+        if(!IsDashing)
+        {
+            rb.velocity = MoveDir * PlayerSpeed;
+        }
+
        // rb.AddForce(MoveDir*PlayerSpeed, ForceMode.VelocityChange);
     }
     private void OnDrawGizmos()
