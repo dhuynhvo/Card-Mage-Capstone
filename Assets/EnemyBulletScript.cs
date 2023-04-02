e15108fbf34d627cf0976c5f92c04834a4ec7e9a
Huynhvo_Dan <dhuynhvo@nevada.unr.edu>
2023-04-02 11:32:42 -0700

    Earthwall

--- Assets/EnemyBulletScript.cs
+++ Assets/EnemyBulletScript.cs
@@ -39,7 +39,7 @@ public class EnemyBulletScript : MonoBehaviour
             other.gameObject.GetComponent<PlayerHealth>().TakeDamage(5);
             Destroy(gameObject);
         }
-        else if(other.gameObject.tag != "Spell" && other.gameObject.tag != "Enemy" && other.gameObject.tag != "Ground" && gameObject.activeSelf)
+        else if(other.gameObject.tag == "Spell" && other.gameObject.tag != "Enemy" && other.gameObject.tag != "Ground" && gameObject.activeSelf)
         {
             Destroy(gameObject);
         }
