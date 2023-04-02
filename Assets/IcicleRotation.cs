5c6098bf3c1283b7ca6b5c174e3cba929dba30b1
Huynhvo_Dan <dhuynhvo@nevada.unr.edu>
2023-04-01 20:00:11 -0700

    Icicle is cool

---
+++ Assets/IcicleRotation.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IcicleRotation : MonoBehaviour
+{
+
+    int angle = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle += 5;
+        transform.rotation = Quaternion.Euler(new Vector3(90f, 0f, angle));
+        if(angle >= 360)
+        {
+            angle = 0;
+        }
+    }
+}
