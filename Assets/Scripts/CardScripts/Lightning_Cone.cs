bccc008108f8ffad84aa93b5a7adb428d39aa924
Huynhvo_Dan <dhuynhvo@nevada.unr.edu>
2023-04-05 17:53:22 -0700

    LIGHTNING + WATER

--- Assets/Scripts/CardScripts/Lightning_Cone.cs
+++ Assets/Scripts/CardScripts/Lightning_Cone.cs
@@ -12,33 +12,8 @@ public class Lightning_Cone : MonoBehaviour
 
     void Start()
     {
-        speed = 15f;
-        dashSpeed = 1.5f;
-    }
-
-    void Update()
-    {
-        if(SpellSpawn != null)
-        {
-            transform.position = SpellSpawn.position + transform.up;
-            transform.rotation = SpellSpawn.rotation;
-        }
-    }
-
-    void OnEnable()
-    {
-        playerMove = GameObject.Find("Player").GetComponent<Player_Movement>();
-        playerRB = GameObject.Find("Player").GetComponent<Rigidbody>();
-        SpellSpawn = GameObject.Find("Player").transform.GetChild(3);
-        playerRB.constraints = RigidbodyConstraints.FreezeAll;
-        playerMove.PlayerSpeed = 0;
-        playerMove.DashSpeed = 0;
-    }
-
-    void OnDisable()
-    {
-        playerRB.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY;
-        playerMove.PlayerSpeed = speed;
-        playerMove.DashSpeed = dashSpeed;
+        Ray rayCast = Camera.main.ScreenPointToRay(Input.mousePosition);
+        transform.position = rayCast.GetPoint(35);
+        Debug.Log(transform.position);
     }
 }
