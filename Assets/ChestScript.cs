8b43f8fbe7d93c87cd116b8c029a515b97e2700e
Huynhvo_Dan <dhuynhvo@nevada.unr.edu>
2023-04-12 21:33:14 -0700

    dwefrgedtfhcy

--- Assets/ChestScript.cs
+++ Assets/ChestScript.cs
@@ -56,16 +56,8 @@ public class ChestScript : MonoBehaviour
             //int randCard = Random.Range(0, CardPool.cards.Count);
             //int priceOfCard = CardPool.cards[randCard].GetComponent<Connected_Spell>().SpellInfo.SpellPrice;
             //player.GetComponent<Player_Currency>().Steves.money -= priceOfCard;
-            var randPosition = new Vector3(0, 0, 0);
-            if (CardNumber == 1)
-            {
-                randPosition = new Vector3(-1.5f, 0, 0);
-            }
-            else if (CardNumber == 2)
-            {
-                randPosition = new Vector3(1.5f, 0, 0);
-            }
-            GameObject Card = Instantiate(CardPool.cards[randCard], gameObject.transform.position, Quaternion.Euler(90, 0, 0));
+            var randPosition = new Vector3(Random.Range(-1.5f, 1.5f), 0, Random.Range(-1.5f, 1.5f));
+            GameObject Card = Instantiate(CardPool.cards[randCard], gameObject.transform.position + randPosition, Quaternion.Euler(90, 0, 0));
             Debug.Log("PURCHASE SUCCESSFUL");
             gameObject.SetActive(false);
             CardSprite.SetActive(false);
