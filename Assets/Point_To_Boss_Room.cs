76a03279c2ff7e4a97868f2b2c3ce005529ad1ec
Huynhvo_Dan <dhuynhvo@nevada.unr.edu>
2023-04-12 23:27:07 -0700

    Spell Spawn is now invisible, pointer to boss room

---
+++ Assets/Point_To_Boss_Room.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class Point_To_Boss_Room : MonoBehaviour
+{
+    public GameObject Target;
+    public bool FoundTarget;
+    public Transform PlayerAvatar;
+    public float radius;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(FoundTarget == true)
+        {
+            transform.LookAt(Target.transform);
+        }
+    }
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
+}
--- Assets/Teleporter_To_Boss.cs
+++ Assets/Teleporter_To_Boss.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Teleporter_To_Boss : MonoBehaviour
 {
     public GameObject BossRoomTeleport;
+    public Point_To_Boss_Room Arrow;
 
     public void Start()
     {
         BossRoomTeleport = GameObject.Find("BossTeleport");
-        
+        Arrow = GameObject.Find("Player").transform.GetChild(5).GetComponent<Point_To_Boss_Room>();
+        Arrow.Target = gameObject.transform.parent.gameObject;
+        Arrow.FoundTarget = true;
     }
 
     private void OnCollisionEnter(Collision other)
@@ -18,6 +21,8 @@ public class Teleporter_To_Boss : MonoBehaviour
         if(other.gameObject.tag == "Player")
         {
             other.transform.position = new Vector3(BossRoomTeleport.transform.position.x, 1f, BossRoomTeleport.transform.position.z);
+            GameObject BossArrow = other.gameObject.transform.GetChild(5).gameObject;
+            BossArrow.SetActive(false);
         }
     }
 }
