d762e7b5d16b07a3c23f4e26e3b348c6242073d9
Huynhvo_Dan <dhuynhvo@nevada.unr.edu>
2023-04-12 21:20:02 -0700

    dwasfewdrgxthcvjinlubkvjchgdv

--- Assets/ChestScript.cs
+++ Assets/ChestScript.cs
@@ -27,19 +27,9 @@ public class ChestScript : MonoBehaviour
     {
         thisID = gameObject.GetInstanceID();
         GameEvents.current.OnShopBuy += Current_OnChestBuy;
-        int q = CardPool.cards.Count / 2;
-        if (CardNumber == 1) //Hard Coded until i find a better solution to overlapping cards
-        {
-            randCard = Random.Range(0, q);
-        }
-
-        else if (CardNumber == 2)
-        {
-            randCard = Random.Range(q + 1, CardPool.cards.Count);
-        }
-
+        randCard = Random.Range(0, CardPool.cards.Count);
         priceOfCard = CardPool.cards[randCard].GetComponent<Connected_Spell>().SpellInfo.SpellPrice;
-        //sprite.sprite = CardPool.cards[randCard].GetComponent<SpriteRenderer>().sprite;
+        sprite.sprite = CardPool.cards[randCard].GetComponent<SpriteRenderer>().sprite;
     }
     private void ChestErr()
     {
@@ -65,7 +55,7 @@ public class ChestScript : MonoBehaviour
         {
             //int randCard = Random.Range(0, CardPool.cards.Count);
             //int priceOfCard = CardPool.cards[randCard].GetComponent<Connected_Spell>().SpellInfo.SpellPrice;
-            player.GetComponent<Player_Currency>().Steves.money -= priceOfCard;
+            //player.GetComponent<Player_Currency>().Steves.money -= priceOfCard;
             var randPosition = new Vector3(0, 0, 0);
             if (CardNumber == 1)
             {
@@ -75,10 +65,10 @@ public class ChestScript : MonoBehaviour
             {
                 randPosition = new Vector3(1.5f, 0, 0);
             }
-            GameObject Card = Instantiate(CardPool.cards[randCard], gameObject.transform.position + randPosition, Quaternion.Euler(90, 0, 0));
+            GameObject Card = Instantiate(CardPool.cards[randCard], gameObject.transform.position, Quaternion.Euler(90, 0, 0));
             Debug.Log("PURCHASE SUCCESSFUL");
             gameObject.SetActive(false);
-            //CardSprite.SetActive(false);
+            CardSprite.SetActive(false);
             PriceObject.SetActive(false);
         }
     }
