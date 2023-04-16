99f1d3c87bad30127e31afc02aa4d6f9c6e8553a
Huynhvo_Dan <dhuynhvo@nevada.unr.edu>
2023-04-15 20:35:39 -0700

    Smoke form rework

--- Assets/Smoke_Form_Behavior.cs
+++ Assets/Smoke_Form_Behavior.cs
@@ -7,14 +7,16 @@ public class Smoke_Form_Behavior : MonoBehaviour
     public GameObject Player;
     public SphereCollider SmokeForm;
     public CapsuleCollider NormalForm;
-
+    
     void Start()
     {
+        int Smoke = LayerMask.NameToLayer("SmokeForm");
         Player = GameObject.Find("Player");
-        SmokeForm = Player.GetComponent<SphereCollider>();
-        NormalForm = Player.GetComponent<CapsuleCollider>();
-        SmokeForm.enabled = true;
-        NormalForm.enabled = false;
+        Player.layer = Smoke;
+        //SmokeForm = Player.GetComponent<SphereCollider>();
+        //NormalForm = Player.GetComponent<CapsuleCollider>();
+        //SmokeForm.enabled = true;
+        //NormalForm.enabled = false;
         transform.rotation = Quaternion.Euler(90, 0, 0);
     }
 
@@ -25,7 +27,9 @@ public class Smoke_Form_Behavior : MonoBehaviour
 
     private void OnDisable()
     {
-        SmokeForm.enabled = false;
-        NormalForm.enabled = true;
+        int Playerr = LayerMask.NameToLayer("Player");
+        //SmokeForm.enabled = false;
+        //NormalForm.enabled = true;
+        Player.layer = Playerr;
     }
 }
