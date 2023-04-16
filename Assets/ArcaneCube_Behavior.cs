e0dcf4119b87dbdcca26061496c4c3ed818115e6
Huynhvo_Dan <dhuynhvo@nevada.unr.edu>
2023-04-15 21:10:19 -0700

    Gravity on cards now

--- Assets/ArcaneCube_Behavior.cs
+++ Assets/ArcaneCube_Behavior.cs
@@ -17,26 +17,26 @@ public class ArcaneCube_Behavior : MonoBehaviour
 
     void FixedUpdate()
     {
-        if(target != null && target.gameObject.tag == "Enemy")
-        {
-            // Get Angle in Radians
-            float AngleRad = Mathf.Atan2(target.transform.position.y - transform.position.y, target.transform.position.z - transform.position.z);
-            // Get Angle in Degrees
-            float AngleDeg = (180 / Mathf.PI) * AngleRad;
-            // Rotate Object
-            if(target.transform.position.x >= transform.position.x)
-            {
-                this.transform.rotation = Quaternion.Euler(90, 0, AngleDeg);
-            }
+        //if(target != null && target.gameObject.tag == "Enemy")
+        //{
+        //    // Get Angle in Radians
+        //    float AngleRad = Mathf.Atan2(target.transform.position.y - transform.position.y, target.transform.position.z - transform.position.z);
+        //    // Get Angle in Degrees
+        //    float AngleDeg = (180 / Mathf.PI) * AngleRad;
+        //    // Rotate Object
+        //    if(target.transform.position.x >= transform.position.x)
+        //    {
+        //        this.transform.rotation = Quaternion.Euler(90, 0, AngleDeg);
+        //    }
 
-            else
-            {
-                this.transform.rotation = Quaternion.Euler(90, 0, -AngleDeg);
-            }
+        //    else
+        //    {
+        //        this.transform.rotation = Quaternion.Euler(90, 0, -AngleDeg);
+        //    }
             
-        }
+        //}
 
-        else if (target != null && target.gameObject.tag == "Boss")
+        if (target != null)
         {
             var speed = GetComponent<Spell_Info>().speed;
             var step = speed * Time.deltaTime; // calculate distance to move
