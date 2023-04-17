f379a11bae03cc4f9a5a7f91ef3b6a9ea1f03eb9
Rbothne <70356932+Rbothne@users.noreply.github.com>
2023-04-16 19:00:11 -0700

    s
    s

--- Assets/Scripts/EnemyScripts/boss/BossBullet.cs
+++ Assets/Scripts/EnemyScripts/boss/BossBullet.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BossBullet : MonoBehaviour
 {
+    public GameObject player;
     private Rigidbody rb;
     public float force;
     private float timer;
@@ -11,11 +12,11 @@ public class BossBullet : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
+        player = GameObject.FindGameObjectWithTag("Player");
         // Use the bullet's forward direction for velocity instead of player direction
         Vector3 direction = transform.forward;
         rb.velocity = direction * force;
-
+        transform.position = new Vector3(transform.position.x, player.transform.position.y, transform.position.z);
         // No rotation calculation is needed since we are not aiming at the player
         float rot = Mathf.Atan2(-direction.y, -direction.x)* Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(90, 90, rot + 0);
